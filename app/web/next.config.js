8018836fbcb22d251d32a21aa05c4eb4535dacd3
Your Name <you@example.com>
2026-03-16 10:31:29 -0600

    Remove localhost-only serverActions config for Vercel

--- app/web/next.config.js
+++ app/web/next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', 'localhost:3001'],
-    },
-  },
 }
 
 module.exports = nextConfig
